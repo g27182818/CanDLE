a3e8dd1782af35a58a8617ef5fb98007a8b1c7a5
g27182818 <gm.mejia@uniandes.edu.co>
2022-04-27 02:38:19 +0000

    Added fiter datasets function

--- test.py
+++ test.py
@@ -5,12 +5,74 @@ import os
 import time
 from utils import *
 
-start = time.time()
-matrix_data = pd.read_feather(os.path.join("data", "final_toil", "data_matrix.feather"))
-categories = pd.read_csv(os.path.join("data", "final_toil", "categories.csv"), encoding = "cp1252")
-phenotypes = pd.read_csv(os.path.join("data", "final_toil", "phenotypes.csv"), encoding = "cp1252")
-end = time.time()
 
-print("Time to load data: {}".format(end - start))
+def read_toil_data(path):
+    """
+    Reads data from the Toil data set with root path and returns matrix_data, categories and phenotypes dataframes.
 
-breakpoint()
+    Args:
+        path (str): Root path of the Toil data set.
+
+    Returns:
+        matrix_data (pd.dataframe): Matrix data of the Toil data set. Columns are samples and rows are genes.
+        categories (pd.dataframe): Categories of the Toil data set. Rows are samples.
+        phenotypes (pd.dataframe): Detailed phenotypes of the Toil data set. Rows are samples.
+    """
+    start = time.time()
+    matrix_data = pd.read_feather(os.path.join(path, "data_matrix.feather"))
+    categories = pd.read_csv(os.path.join(path, "categories.csv"), encoding = "cp1252")
+    phenotypes = pd.read_csv(os.path.join(path, "phenotypes.csv"), encoding = "cp1252")
+    # Delete the first column of categories and phenotypes
+    categories = categories.drop(categories.columns[0], axis = 1)
+    phenotypes = phenotypes.drop(phenotypes.columns[0], axis = 1)
+    end = time.time()
+    print("Time to load data: {} s".format(round(end - start, 3)))
+    return matrix_data, categories, phenotypes
+
+
+def filter_toil_datasets(matrix_data, categories, phenotypes, tcga = True, gtex = True):
+    """
+    Filters the Toil data set by using or not using TCGA and GTEx samples.
+
+    Args:
+        matrix_data (pd.dataframe): Dataframe of the complete Toil data set obtained from read_toil_data().
+        categories (pd.dataframe): Dataframe of categories of the complete Toil data set obtained from read_toil_data().
+        phenotypes (pd.dataframe): Dataframe of phenotypes of the complete Toil data set obtained from read_toil_data().
+        tcga (bool, optional): If True, TCGA samples are used. Defaults to True. 
+        gtex (bool, optional): If True GTEX samples are used. Defaults to True.
+
+    Raises:
+        ValueError: If both tcga and gtex are False then an error is raised because there is no data to use.
+
+    Returns:
+        matrix_data_filtered(pd.dataframe): Dataframe of the filtered Toil data set.
+        categories_filtered(pd.dataframe): Dataframe of the filtered categories of the Toil data set.
+        phenotypes_filtered(pd.dataframe): Dataframe of the filtered phenotypes of the Toil data set.
+    """
+    # Filter out all TARGET samples from matrix_data
+    matrix_data_filtered = matrix_data.iloc[:, ~matrix_data.columns.str.contains("TARGET")]
+
+    # Handle the filters for TCGA and GTEx
+    if tcga and (~gtex):
+        # Filter out all gtex samples from matrix_data
+        matrix_data_filtered = matrix_data_filtered.iloc[:, ~matrix_data_filtered.columns.str.contains("GTEX")]
+        categories_filtered = categories[~categories["sample"].str.contains("GTEX")]
+        phenotypes_filtered = phenotypes[~phenotypes["sample"].str.contains("GTEX")]
+    elif (~tcga) and gtex:
+        # Filter out all tcga samples from matrix_data
+        matrix_data_filtered = matrix_data_filtered.iloc[:, ~matrix_data_filtered.columns.str.contains("TCGA")]
+        categories_filtered = categories[~categories["sample"].str.contains("TCGA")]
+        phenotypes_filtered = phenotypes[~phenotypes["sample"].str.contains("TCGA")]
+    elif tcga and gtex:
+        # Do nothing because both TCGA and GTEX samples are included
+        pass
+    else:
+        raise ValueError("You are not selecting any dataset.")
+        
+    return matrix_data_filtered, categories_filtered, phenotypes_filtered 
+
+
+test_matrix_data, test_categories, test_phenotypes = read_toil_data(os.path.join("data", "toil_data"))
+x_np, y_np, sample2loc, label2cat = filter_toil_datasets(test_matrix_data, test_categories, test_phenotypes)
+
+
--- toil_downloader.R
+++ toil_downloader.R
@@ -1,6 +1,10 @@
 library("UCSCXenaTools")
 library("feather")
-# setwd("C:/Users/Usuario")
+
+# Print Masage to the user
+cat("Starting download of the data\nThis should take approximately 10 minutes...\n\n\n\n")
+# Make pause for 10 seconds to let the user read the message
+Sys.sleep(10)
 
 # This sets an environment variable to let R load the huge dataset
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 4)
@@ -15,19 +19,26 @@ xe <- XenaHub(hostName = "toilHub",
 # Define the Xena query object
 xe_query <- XenaQuery(xe)
 
+
 # Download data
-xe_download <- XenaDownload(xe_query, destdir = file.path("data", "toil"), max_try = 10L)
+cat("Downloading data...\n")
+xe_download <- XenaDownload(xe_query, destdir = file.path("data", "toil_download"), max_try = 10L)
 # Load data into R
+cat("Loading data into R and un...\n")
 dat <- XenaPrepare(xe_download, chunk_size = 100)
 
 data_matrix <- dat$TcgaTargetGtex_rsem_gene_tpm.gz
 categories <- dat$TCGA_GTEX_category.txt
 phenotypes <- dat$TcgaTargetGTEX_phenotype.txt.gz
 
-
 # create data directory
-dir.create(file.path("data", "final_toil"), recursive = TRUE, showWarnings = FALSE)
+dir.create(file.path("data", "toil_data"), recursive = TRUE, showWarnings = FALSE)
 
-write_feather(data_matrix, file.path("data", "final_toil", "data_matrix.feather"))
-write.csv(categories, file.path("data", "final_toil", "categories.csv"))
-write.csv(phenotypes, file.path("data", "final_toil", "phenotypes.csv"))
+# Save data
+cat("Writing data to", file.path("data", "toil_data"), "directory\n")
+write_feather(data_matrix, file.path("data", "toil_data", "data_matrix.feather"))
+write.csv(categories, file.path("data", "toil_data", "categories.csv"))
+write.csv(phenotypes, file.path("data", "toil_data", "phenotypes.csv"))
+
+# Delete the toil_download directory
+unlink(file.path("data", "toil_download"), recursive = TRUE, force = TRUE)
